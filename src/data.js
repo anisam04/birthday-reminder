bdbd65efb2875599b7b5c6fec46d2d717deac053
ani0104 <annie.samajpati@gmail.com>
2022-11-14 15:25:43 -0700

    changes in package.json

--- src/data.js
+++ src/data.js
@@ -34,18 +34,6 @@ export default [
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
   },
-  {
-    id: 5,
-    name: 'Lola Gardner',
-    age: 29,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-  },
-  {
-    id: 7,
-    name: 'Pinky Pig',
-    age: 29,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-  }
+  
+  
 ];
