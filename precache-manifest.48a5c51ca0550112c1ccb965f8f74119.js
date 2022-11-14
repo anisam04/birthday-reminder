44eaedf464c94e9e24157c74e19f8745fc5d8f22
ani0104 <annie.samajpati@gmail.com>
2022-11-14 15:26:13 -0700

    Updates

---
+++ precache-manifest.48a5c51ca0550112c1ccb965f8f74119.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20555c524d14e831f3f9d05bd32c2fd5",
+    "url": "/birthday-reminder/index.html"
+  },
+  {
+    "revision": "5a1ad63f1afbfb3bcab1",
+    "url": "/birthday-reminder/static/css/main.02d11260.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a1ad63f1afbfb3bcab1",
+    "url": "/birthday-reminder/static/js/main.c7baf607.chunk.js"
+  },
+  {
+    "revision": "5567b67584bb31e15f4d",
+    "url": "/birthday-reminder/static/js/runtime-main.35ffc63d.js"
+  }
+]);
